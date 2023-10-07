31d3830c13d5da2038cacca2469fc8abe0a728d1
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2023-10-07 13:03:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 module.exports = {
-  telegraf_token: "5732023365:AAHN0rbrL1Fm-Ytnp7E0Q0vsAGuEPLirnO4",
+  telegraf_token: "6656748587:AAF4BvJWAtFrvn5fY2Y36HvkYdsUSebATHA",
   mongoURL: "mongodb+srv://Lucifer:Lucifer@cluster0.uh7tqik.mongodb.net/",
 };
